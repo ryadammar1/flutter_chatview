482aaee17d388d11fe61245518ee6fe4e9f15c90
ryadammar1 <mohamed.ammar@mail.mcgill.ca>
2024-11-04 22:58:10 -0500

    outgoing message alignment, added back swipe to reply

--- lib/src/widgets/chat_bubble_widget.dart
+++ lib/src/widgets/chat_bubble_widget.dart
@@ -269,40 +269,44 @@ class _ChatBubbleWidgetState extends State<ChatBubbleWidget> {
                   onTap: () => widget.onReplyTap
                       ?.call(widget.message.replyMessage.messageId),
                 ),
-        MessageView(
-          outgoingChatBubbleConfig:
-              chatListConfig.chatBubbleConfig?.outgoingChatBubbleConfig,
-          isLongPressEnable:
-              (featureActiveConfig?.enableReactionPopup ?? true) ||
-                  (featureActiveConfig?.enableReplySnackBar ?? true),
-          inComingChatBubbleConfig:
-              chatListConfig.chatBubbleConfig?.inComingChatBubbleConfig,
-          message: widget.message,
-          isMessageBySender: isMessageBySender,
-          messageConfig: chatListConfig.messageConfig,
-          onLongPress: widget.onLongPress,
-          chatBubbleMaxWidth: chatListConfig.chatBubbleConfig?.maxWidth,
-          longPressAnimationDuration:
-              chatListConfig.chatBubbleConfig?.longPressAnimationDuration,
-          onDoubleTap: featureActiveConfig?.enableDoubleTapToLike ?? false
-              ? chatListConfig.chatBubbleConfig?.onDoubleTap ??
-                  (message) => currentUser != null
-                      ? chatController?.setReaction(
-                          emoji: heart,
-                          messageId: message.id,
-                          userId: currentUser!.id,
-                        )
-                      : null
-              : null,
-          shouldHighlight: widget.shouldHighlight,
-          controller: chatController,
-          highlightColor: chatListConfig.repliedMessageConfig
-                  ?.repliedMsgAutoScrollConfig.highlightColor ??
-              Colors.grey,
-          highlightScale: chatListConfig.repliedMessageConfig
-                  ?.repliedMsgAutoScrollConfig.highlightScale ??
-              1.1,
-          onMaxDuration: _onMaxDuration,
+        SwipeToReply(
+          isMessageByCurrentUser: isMessageBySender,
+          onSwipe: isMessageBySender ? onLeftSwipe : onRightSwipe,
+          child: MessageView(
+            outgoingChatBubbleConfig:
+                chatListConfig.chatBubbleConfig?.outgoingChatBubbleConfig,
+            isLongPressEnable:
+                (featureActiveConfig?.enableReactionPopup ?? true) ||
+                    (featureActiveConfig?.enableReplySnackBar ?? true),
+            inComingChatBubbleConfig:
+                chatListConfig.chatBubbleConfig?.inComingChatBubbleConfig,
+            message: widget.message,
+            isMessageBySender: isMessageBySender,
+            messageConfig: chatListConfig.messageConfig,
+            onLongPress: widget.onLongPress,
+            chatBubbleMaxWidth: chatListConfig.chatBubbleConfig?.maxWidth,
+            longPressAnimationDuration:
+                chatListConfig.chatBubbleConfig?.longPressAnimationDuration,
+            onDoubleTap: featureActiveConfig?.enableDoubleTapToLike ?? false
+                ? chatListConfig.chatBubbleConfig?.onDoubleTap ??
+                    (message) => currentUser != null
+                        ? chatController?.setReaction(
+                            emoji: heart,
+                            messageId: message.id,
+                            userId: currentUser!.id,
+                          )
+                        : null
+                : null,
+            shouldHighlight: widget.shouldHighlight,
+            controller: chatController,
+            highlightColor: chatListConfig.repliedMessageConfig
+                    ?.repliedMsgAutoScrollConfig.highlightColor ??
+                Colors.grey,
+            highlightScale: chatListConfig.repliedMessageConfig
+                    ?.repliedMsgAutoScrollConfig.highlightScale ??
+                1.1,
+            onMaxDuration: _onMaxDuration,
+          ),
         ),
       ],
     );
